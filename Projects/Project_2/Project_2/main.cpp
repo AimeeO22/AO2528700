450ede364d819a7fb2bea3e6d298c4b3fc051edb
AimeeO22 <aimee.orozco22@gmail.com>
2014-07-28 13:08:01 -0700

    update

--- Projects/Project_2/Project_2/main.cpp
+++ Projects/Project_2/Project_2/main.cpp
@@ -6,14 +6,32 @@
  */
 
 #include <cstdlib>
-
+#include <fstream>
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
+    ofstream outputFile;
+    outputFile. open("demofile.txt");
+    cout<<"Practice 1"<<endl;
+    outputFile<<"Sergiooooo \n";
+    outputFile<<"is a \n";
+    outputFile<<"giant \n";
+    outputFile.close();
+    ifstream inputFile;
+    string name;
+    inputFile.open("demofile.txt");
+    cout<<"Reading file, practice 2\n";
+    inputFile>>name;
+    cout<<name<<endl;
+    inputFile>>name;
+    cout<<name<<endl;
+    inputFile>>name;
+    cout<<name<<endl;
+    inputFile>>name;
+    cout<<name<<endl;
+    inputFile.close();
     return 0;
 }
 

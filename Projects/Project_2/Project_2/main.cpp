eb25b1998f65ab481d25cb760ac3c05364b53245
AimeeO22 <aimee.orozco22@gmail.com>
2014-07-28 11:38:02 -0700

    update

---
+++ Projects/Project_2/Project_2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Aimee Orozco-Perez
+ * Created on July 26, 2014, 10:50 AM
+ * Purpose: Project 2 Game
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
